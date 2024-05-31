2d0205bf550fd62cf8bf2823dbea8dfd201c49ba
Catercodes <chrisadamsadewuni@gmail.com>
2024-05-31 21:07:58 +0100

    Pacal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3pt
+def pascal_triangle(n):
+    result = [[1]]
+    for i in range(n-1):
+        _space = [0] + result[-1] +[0]
+        row = []
+        for j in range(len(result[-1])+1):
+            row.append (_space[j]+_space[j+1])
+        result.append(row)
+    return result
